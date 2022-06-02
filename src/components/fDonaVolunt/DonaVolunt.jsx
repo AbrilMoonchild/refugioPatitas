5e59e63d24f3971cd09407297097ab69984fefe0
AJJR811 <alvarolink888@gmail.com>
2022-06-01 21:32:25 -0400

    base proyecto

---
+++ src/components/fDonaVolunt/DonaVolunt.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DonaVolunt() {
+  return (
+    <div>DonaVolunt</div>
+  )
+}
+
+export {DonaVolunt}
---
+++ src/components/fRefugiados/Refugiados.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Refugiados() {
+  return (
+    <div>Refugiados</div>
+  )
+}
+
+export {Refugiados}
