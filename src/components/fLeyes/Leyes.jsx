b330952539feb76a304d206a961361da4faae469
Alvaro Mallea Perez <alvaromalleaperez@Alvaros-MacBook-Pro.local>
2022-06-02 03:55:15 -0400

    Base de la estructura de Leyes

--- src/components/fLeyes/Leyes.jsx
+++ src/components/fLeyes/Leyes.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import './Leyes.css'
 
 function Leyes() {
   return (
-    <div>Leyes</div>
+    <>
+    
+    <header className='page-title'>
+      <div className='container'>
+        <div className='row'>
+            <h1 className='page-title-center'>Leyes y Normas</h1>
+            <div className='back-home'>
+              <a href='../fHome/Home.jsx'>
+                "Inicio"
+              </a>
+            </div>
+        </div>
+      </div>
+    </header>
+    
+    
+    <div className='leyes-text'>
+      <div classNam='cpe'>
+        <p>"CPE artículo 302 Inc. 5 (año 2009)"</p>
+        <p>"Son competencias exclusivas de los gobiernos municipales autónomos, en su jurisdicción Inc. 5 “Preservar, conservar y contribuir a la protección del medio ambiente y recursos naturales, fauna silvestre y animales domésticos."</p>
+      </div>
+      <div>
+        <p>"Bolivia se adhiere a la DUBA (año 2010)"</p>
+        <p>"El año 2010, Bolivia, mediante cancillería, se adhiere a la DUBA (Declaración Universal del Bienestar Animal)"</p>
+      </div>
+      
+      <p>"01 DE JUNIO DE 2015.- LEY 700 PARA LA DEFENSA DE LOS ANIMALES CONTRA ACTOS DE CRUELDAD Y MALTRATO"</p>
+      <p>"LEY Nº 553 “PERROS PELIGROSOS”"</p>
+      <span>Publicado el 1 de agosto de 2014</span>
+      <p>Ley 4095</p>
+      <span>Publicado en la Edición: </span><span>58NEC </span><span>Fecha de Publicación: </span><span>02/09/2009</span>
+      <p>21 DE AGOSTO DE 2009.- Declara de necesidad y utilidad pública, la construcción de albergues para animales domésticos abandonados en la ciudad de Oruro, a fin de proteger la salud e integridad física de las personas así como el bienestar de los animales.</p>
+      <p>Ley 4040</p>
+      <span>Publicado en la Edición: </span><span>39NEC </span><span>Fecha de Publicación: </span><span>01/07/2009</span>
+      <p>17 DE JUNIO DE 2009.- Elimina el uso de animales silvestres y/o domésticos en espectáculos circenses en todo el territorio nacional, por considerarse la práctica un acto de crueldad en contra de los animales.</p>
+      <div>
+        <p>Ley 1333</p>
+        <span>Fecha de Publicación: </span><span>27/04/1992</span>
+        <span>Artículo 111. </span>
+        <p>El que incite, promueva, capture y/o comercialice el producto de la cacería, tenencia, acopio, transporte de especies animales y vegetales, o de sus derivados sin autorización o que estén declaradas en veda o reserva, poniendo en riesgo de extinción a las mismas, sufrirá la pena de privación de libertad de hasta dos años perdiendo las especies, las que serán devueltas a su hábitat natural, si fuere aconsejable, más la multa equivalente al cien por ciento del valor de estas.</p>
+
+      </div>
+    </div><div className='leyes-pdf'> </div></>
   )
 }
 
