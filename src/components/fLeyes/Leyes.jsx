5347efba0094776b2641b9b105efde177405c80c
Beatriz Aliaga <baliagar@est.emi.edu.bo>
2022-06-02 20:17:46 -0400

    Mejorando la presentación

--- src/components/fLeyes/Leyes.jsx
+++ src/components/fLeyes/Leyes.jsx
@@ -9,26 +9,24 @@ function Leyes() {
       <div className='container'>
         <div className='row'>
             <h1 className='page-title-center'>Leyes y Normas</h1>
-            <div className='back-home'>
-              
-            </div>
+            
         </div>
       </div>
     </header>
     
     
     <div className='leyes-text'>
-      <div classNam='cpe'>
-        <p>"CPE artículo 302 Inc. 5 (año 2009)"</p>
-        <p>"Son competencias exclusivas de los gobiernos municipales autónomos, en su jurisdicción Inc. 5 “Preservar, conservar y contribuir a la protección del medio ambiente y recursos naturales, fauna silvestre y animales domésticos."</p>
+      <div className='cpe'>
+        <p>CPE artículo 302 Inc. 5 (año 2009)</p>
+        <p>Son competencias exclusivas de los gobiernos municipales autónomos, en su jurisdicción Inc. 5 “Preservar, conservar y contribuir a la protección del medio ambiente y recursos naturales, fauna silvestre y animales domésticos.</p>
       </div>
-      <div>
-        <p>"Bolivia se adhiere a la DUBA (año 2010)"</p>
-        <p>"El año 2010, Bolivia, mediante cancillería, se adhiere a la DUBA (Declaración Universal del Bienestar Animal)"</p>
+      <div className='dubas'>
+        <p>Bolivia se adhiere a la DUBA (año 2010)</p>
+        <p>El año 2010, Bolivia, mediante cancillería, se adhiere a la DUBA (Declaración Universal del Bienestar Animal)</p>
       </div>
       
-      <p>"01 DE JUNIO DE 2015.- LEY 700 PARA LA DEFENSA DE LOS ANIMALES CONTRA ACTOS DE CRUELDAD Y MALTRATO"</p>
-      <p>"LEY Nº 553 “PERROS PELIGROSOS”"</p>
+      <p>01 DE JUNIO DE 2015.- LEY 700 PARA LA DEFENSA DE LOS ANIMALES CONTRA ACTOS DE CRUELDAD Y MALTRATO</p>
+      <p>LEY Nº 553 “PERROS PELIGROSOS”</p>
       <span>Publicado el 1 de agosto de 2014</span>
       <p>Ley 4095</p>
       <span>Publicado en la Edición: </span><span>58NEC </span><span>Fecha de Publicación: </span><span>02/09/2009</span>
