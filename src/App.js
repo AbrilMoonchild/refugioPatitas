14cb108b9d4b0f1f996908a7ad41856751784d76
Angela Alarcon <aangelaabril101@gmail.com>
2022-06-03 02:17:48 -0400

    footer Corregido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import {NavBar} from './routes/NavBar.jsx'
 import {Footer} from './routes/Footer.jsx'
-<<<<<<< HEAD
-
-=======
 import './App.css';
 import React from 'react';
->>>>>>> footer
 function App() {
   return (
     <div className="App">
