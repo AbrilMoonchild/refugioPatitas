c35d2c5eae5de8455236b5649c2682e35b456b15
Nickolas-Johan-Moya-Faverman <101344259+Nickolas-Johan-Moya-Faverman@users.noreply.github.com>
2022-06-09 04:42:38 -0400

    Add files via upload

--- src/components/fReqAdop/ReqAdop.jsx
+++ src/components/fReqAdop/ReqAdop.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './ReqAdop.css'
 
 function ReqAdop() {
   return (
-    <div>ReqAdop</div>
+    <section class="textos-header">
+            <h1> REQUISITOS PARA LA ADOPCIÓN DE UNA MASCOTA </h1>
+            <h2>La sangre sólo hace parientes, el amor hace Familia</h2>
+      </section>
   )
 }
 
