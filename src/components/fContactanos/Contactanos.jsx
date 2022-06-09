224430704204c43ef2cd2032a0a13c45e2b94d9e
[carly19] <Carlypau384@gmail.com>
2022-06-09 11:16:44 -0400

    2do Comit trujillo

--- src/components/fContactanos/Contactanos.jsx
+++ src/components/fContactanos/Contactanos.jsx
@@ -1,58 +1,48 @@
 import React from 'react'
-import './contactanos.css'
-import styled from "styled-components";
-
-const Section = styled.section`
-  position: relative;
-  min-height: 100vh;
-  display: flex;
-
-  margin: 0 auto;
-`;
-
-const Title = styled.h1`
-  font-size: ${(props) => props.theme.fontBig};
-  font-family: "Kaushan Script";
-  font-weight: 300;
-
-  position: fixed;
-  top: -0.6rem;
-  left: 24%;
-  z-index: 5;
-
-`;
-
-function Contactanos(props) {
+import '../fContactanos/contactanos.css'
+import'../fContactanos/icon/facebook.png'
+function Contactanos() {
   return (
+    <>
+    <div>
+    <h1>CONTACTOS</h1>
+   
+    <h2>"Nos puedes contactar por nuestras redes sociales , o visitando nuestro establecimiento"</h2>
+   
+    </div>
 
-    <Section>
-    <Title>
-      Sobre Nosotros
-    </Title>
+    <div className='contenedor-casa'>
+      <div>
+        <h2>Dirección Casa Matriz:</h2>
+        <p>Escuela militar de ingenieria , Calle 2 de irpavi(LADO MEGACENTER)</p>
+      </div>
+      <div>
+        <h2>Telefono:</h2>
+        <p>2724850/ Fax : 2724952</p>
+      </div>
+      <div>
+        <h2>WhatsAp</h2>
+        <p>Cel: 70150215</p>
+        <p>Cel: 76526738</p>
+      </div>
+      <div>
+        <h2>Facebook</h2>
+        <p>Pagina Oficial: Refugio Patitas</p>
+      </div>
+      <div>
+        <h2>Instagram</h2>
+        <p>Refugio Patias 1_0_EMI</p>
+      </div>
+    </div>
 
-   <div className="card text-center">
-     <div className="overflow">
-       
-     </div>
-     <div className="card-body text-dark">
-       <h4 className="card-title">{props.title}</h4>
-       <p className="card-text text-secondary">
+    <div className='contenedor-logos'>
+      <div>
+        <img src='/src/components/fContactanos/icon/facebook.png' alt="Foto Facebook"></img>
+
+      </div>
+      </div>
       
-       Nos puedes encontrar en nuestras redes sociales.- 
-Contactos
- Facebook .- PAGINA OFICIAL PATITAS<img src="../fContacts/icon/facbook.jpg"></img>
- WHATSAPP .- +591 70150215
- INTAGRAM .- PATITAS REFUGIO  <img src="icon/instagram.png"></img>
- Nuesta direccion:
- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3825.028965055029!2d-68.1282544848185!3d-16.52463528860101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915f21f62cf95b27%3A0xc9e60dac45aa6851!2sAlbergue%20de%20Mascotas%20%22Peluch%C3%ADn%22!5e0!3m2!1ses-419!2sbo!4v1654219739744!5m2!1ses-419!2sbo" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-       </p>
-
-       <a href="#" className="btn btn-outline-success">TE ESPERAMOS</a>
-       </div> 
-       </div> 
-   </Section>
-  );
-}
+  </>)  }
+      
 
 export {Contactanos}
-    
